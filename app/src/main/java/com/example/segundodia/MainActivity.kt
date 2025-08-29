f9a377f48467c02fc3c41a0d65628ffc9ccd7028
Mxryz <maryjosedoblas@gmail.com>
2025-08-29 13:34:01 -0600

    se agregó nombre

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SegundoDiaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android", //Maryjose Quiñones
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
