d49da44ada19905964d600d9b6ec083e187a0ffb
alfredo <alfredo.puentes25@outlook.com>
2025-08-29 13:20:11 -0600

    Se agrego numero de control

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SegundoDiaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android", //Alfredo Puentes Vargas 22130803
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
