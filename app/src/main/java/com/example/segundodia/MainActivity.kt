ae08017cc21b5fb8637407ed691353f72d158bb2
Chr17s <crishdz690@gmail.com>
2025-08-29 13:20:43 -0600

    modificacion nombre

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SegundoDiaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android", //Christian Hernandez
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
