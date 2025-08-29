30a43cc70d77ef6740cc4b886c269b451c78059c
abr4hamw <abraham4woods@gmail.com>
2025-08-29 13:20:54 -0600

    mi nombre al lado de "Adroid"

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SegundoDiaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android", //Abraham Jiménez
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
