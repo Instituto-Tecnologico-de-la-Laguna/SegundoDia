29aabbda25bad4b82cac88e8d0ccebcb617593d5
aaisforgg <aaliyahgallardo13105@gmail.com>
2025-09-01 13:50:54 -0600

    Creación de elementos tipo texto y manejo de interfaz por columnas y filas

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.segundodia.ui.theme.SegundoDiaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +24,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android", //Aaliyah Gallardo García #22130821
+                        clase = "Desarrollo android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +34,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String, clase: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            fontSize = 50.sp,
+        )
+        Text(
+            text = "Jose Luis",
+            fontSize = 30.sp,
+
+        )
+        Text(
+            text = "$clase",
+            fontSize = 30.sp,
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SegundoDiaTheme {
-        Greeting("Android")
+        Greeting("Android", clase = "Desarrollo Android")
     }
 }
