e8cae6a3249c156ab2147d2fccb7a95a3b677c20
apalo <apalomo809@gmail.com>
2025-09-01 13:51:09 -0600

    Creacion de elementos tipo texto texto y manejo de interfaz en columnas y fias

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.segundodia.ui.theme.SegundoDiaTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,10 +36,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        fontSize = 30.sp,
+
     )
+
+    Text(
+        text = "Angel Palomo",
+        fontSize = 30.sp,
+
+    )
+
+    Text(
+        text = "Desarollo Android",
+        fontSize = 30.sp,
+
+    )
+    }
 }
 
 @Preview(showBackground = true)
