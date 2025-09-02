98fb10cd6fbafc734aef0ae2f79e24984fa834ba
Mxryz <maryjosedoblas@gmail.com>
2025-09-02 13:36:48 -0600

    ..

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -22,13 +26,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SegundoDiaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color= MaterialTheme.colorScheme()
+                ){
+                    GreetingText(
                         name = "Android",
-                        clase="Desarrollo Android",
+                        clase = "Desarrollo Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+            }
             }
         }
     }
@@ -64,7 +73,10 @@ fun GreetingPreview() {
 
 @Composable
 fun GreetingText(message:String, from: String,modifier:Modifier) {
-    Row{
+   Column(
+       verticalArrangement=Arrangement.Center,
+       horizontalAlignment = Alignment.CenterHorizontally
+   ){
         Text(
             text=message,
             fontSize = 118.sp,
