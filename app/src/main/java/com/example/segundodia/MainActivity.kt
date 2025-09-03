f68059864de71f82a681c225b602fe9b21a2de6a
aaisforgg <aaliyahgallardo13105@gmail.com>
2025-09-03 13:54:03 -0600

    Uso de textFields

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.segundodia
 
 import android.R
+import android.R.attr.label
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,13 +11,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.segundodia.ui.theme.SegundoDiaTheme
@@ -27,67 +39,49 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SegundoDiaTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Hola Mundo", "Jose Luis")
 
+                Inicio()
                 }
             }
         }
     }
-}
+
 
 @Composable
-fun Greeting(name: String, clase: String, modifier: Modifier = Modifier) {
-    Column {
+fun Inicio(){
+    var texto1 by remember { mutableStateOf("") }
+    var texto2 by remember { mutableStateOf("") }
+    var texto3 by remember { mutableStateOf("") }
+    var texto4 by remember { mutableStateOf("") }
+    var texto5 by remember { mutableStateOf("") }
 
-        Text(
-            text = "Hello $name!",
-            modifier = modifier,
-            fontSize = 50.sp,
-        )
-        Text(
-            text = "Jose Luis",
-            fontSize = 30.sp,
-
-        )
-        Text(
-            text = "$clase",
-            fontSize = 30.sp,
-
-        )
+    Column() {
+        Row() {
+            //textField Sencillo
+            TextField(value = texto1, onValueChange = { nuevoTexto -> texto1 = nuevoTexto },)
+        }
     }
-    Column(
-        content={
-        Text(text="Hola Mundo")
-        Text(text="Expresión en content")}
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SegundoDiaTheme {
-        Greeting("Android", clase = "Desarrollo Android")
+    Row(){
+        //textField con label y placeHolder
+        TextField(value=texto2,onValueChange = {texto2=it}, label = {Text("Nombre")},
+            placeholder = {Text("Escribe tu nombre")})
     }
-}
-@Composable
-fun GreetingText(message: String, from:String, modifier:Modifier){
-   Column(
-       verticalArrangement = Arrangement.Center,
-       horizontalAlignment = Alignment.CenterHorizontally
-   ){
-        Text(
-            text =message,
-            fontSize = 50.sp,
-            lineHeight = 116.sp
-        )
+    Row(){
+        //Keyboard Options
+        TextField(value = texto3, label={Text("telefono")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone), onValueChange={ it -> texto3=it})
+    }
 
-       Text(
-           text =from,
-           fontSize = 30.sp
-       )
-   }
+    Row(){
+        //OutlinedTextField
+        OutlinedTextField(value=texto4,label={Text("Correo")}, onValueChange = {texto4=it})
+    }
+
+    Row(){
+        //OutlinedTextField Con Icono
+       OutlinedTextField(value = texto5, leadingIcon = {Icon(imageVector = Icons.Default.Email,
+           contentDescription = "Email Icon")}, onValueChange = {texto5=it}, label={Text("Correo")},
+           placeholder = {Text("Escribe tu correo")})
+    }
+
 }
