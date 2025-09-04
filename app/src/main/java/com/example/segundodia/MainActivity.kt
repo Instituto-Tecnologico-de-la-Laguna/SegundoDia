bd6c4171794cdf619cd47f064cda4ac0e196342e
jmot4 <jmota@gtech-services.com>
2025-09-04 13:51:33 -0600

    Se agregan botones en una nueva rama.

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,8 +19,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 import com.example.segundodia.ui.theme.SegundoDiaTheme
@@ -32,10 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
-                        "Hola Mundo",
-                        "Jose Luis",
-                    )
+                   Botones()
                 }
 
             }
@@ -44,59 +46,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String,clase:String ,modifier: Modifier = Modifier) {
-    Column(
-
-    ) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        Text(
-            text = "Jose Luis",
-            fontSize = 30.sp
-        )
-        Text(
-            text = "$clase",
-            fontSize = 30.sp
-
-        )
-    }
-    Column(
-        content={
-            Text("Hola Mundo")
-            Text("Expresion en Content")
+fun Botones() {
+    Column(modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(modifier=Modifier.padding(20.dp)) {
+            Button(onClick = { /*TODO*/ }
+            ) {
+                Text("Boton Simple")
+            }
         }
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SegundoDiaTheme {
-        Greeting("Android","Desarrollo Android")
+        Row(){
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors( Color.Gray)){
+                Text("Boton con Color")
+            }
+        }
     }
 }
-
-@Composable
-fun GreetingText(message:String,from:String, modifier: Modifier = Modifier) {
-
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = message,
-            fontSize = 110.sp,
-            lineHeight = 116.sp,
-        )
-        Text(
-            text = from,
-            fontSize = 30.sp
-        )
-    }
-
-
-    }
-
-
