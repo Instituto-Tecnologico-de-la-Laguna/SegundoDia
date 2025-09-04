ecb8eecd9a732b814bfbdd872c34eaf6dd4da2c0
Victor Melendez <vicomelbass@gmail.com>
2025-09-04 13:51:06 -0600

    Se agregaron botones en nueva rama

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.segundodia
 
+import android.R.attr.background
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.segundodia.ui.theme.SegundoDiaTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +27,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SegundoDiaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+
+                   Botones()
                 }
             }
         }
     }
-}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SegundoDiaTheme {
-        Greeting("Android")
+fun Botones(){
+    Column(modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(modifier = Modifier.padding(20.dp)) {
+            Button(onClick = { /* TODO*/ })
+            {
+                Text("Boton simple")
+            }
+        }
+        Row() {
+            Button(onClick = { /* TODO*/ },
+                colors = ButtonDefaults.buttonColors(Color.Gray))
+            {
+                Text("Boton simple")
+            }
+        }
     }
 }
