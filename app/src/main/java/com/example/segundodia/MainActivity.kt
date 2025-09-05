99cb30b4101f17ca7b677e34254de1670e133115
jmot4 <jmota@gtech-services.com>
2025-09-05 13:19:52 -0600

    Botones 2 cambios 1

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -21,6 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,9 +51,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
 fun Botones() {
-    Column(modifier = Modifier,
+    Column(modifier = Modifier
+        .fillMaxWidth(1f),
         horizontalAlignment = Alignment.CenterHorizontally) {
         Row(modifier=Modifier.padding(20.dp)) {
             Button(onClick = { /*TODO*/ }
@@ -58,8 +66,24 @@ fun Botones() {
 
         Row(){
             Button(onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors( Color.Gray)){
+                colors = ButtonDefaults.buttonColors( Color.Red)){
                 Text("Boton con Color")
+            }
+        }
+        Row(){
+            Button(onClick = { /*TODO*/ }) {
+                Text("Boton ",color=Color.Red)
+                Text("Colores",color=Color.Yellow)
+            }
+        }
+        Row(){
+            Button(onClick = { /*TODO*/ }){
+                Image(
+                    painterResource(id = R.drawable.ic_cart),
+                    contentDescription = TODO()
+                )
+
+
             }
         }
     }
