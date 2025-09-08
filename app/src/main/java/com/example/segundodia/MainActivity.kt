2e9385aff7ab66ac735f2213db77e1edb6ea4652
Mxryz <maryjosedoblas@gmail.com>
2025-09-08 13:25:25 -0600

    n

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.segundodia.ui.theme.SegundoDiaTheme
 
@@ -44,7 +45,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+@Preview(showBackground = true)
 @Composable
 fun Botones(){
     Column (modifier = Modifier.fillMaxWidth(1f),
