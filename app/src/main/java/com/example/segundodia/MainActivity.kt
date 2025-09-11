44aad86372760a1e5d81b51aa1f9fea674fb2191
Mxryz <maryjosedoblas@gmail.com>
2025-09-11 12:11:28 -0600

    n

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -1,33 +1,35 @@
 package com.example.segundodia
-import android.R
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.segundodia.ui.theme.SegundoDiaTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,37 +40,78 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                  MainScreen()
+                    val focusManager = LocalFocusManager.current
+                    val items = listOf(
+                        Color.Blue,
+                        Color.Black,
+                        Color.Cyan,
+                        Color.Yellow,
+                        Color.Red,
+                        Color.Gray,
+                        Color.Green,
+                        Color.DarkGray,
+                        Color.Magenta
+                    )
+                    MainScreen(items)
                 }
 
             }
         }
     }
 }
-@Preview(showBackground = true)
+
+
 @Composable
-fun MainScreen() {
-    Column(){
-        Row(){
+fun MainScreen(items : List<Color>) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row() {
             Image(
-                painterResource(id=R.drawable.ic_input_delete),
-                contentDescription = null)
+                painter = painterResource(id = R.drawable.criss),
+                contentDescription = null
+            )
+        }
+        Row(){
+            Texto("Clorox",
+                Color.Blue,
+                Color.White)
+            Spacer(modifier = Modifier.padding(10.dp))
+            Texto ("Precio",
+                Color.Red,
+                Color.White)
+        }
+        Spacer(modifier = Modifier.padding(20.dp))
+        LazyRow() {
+            items(items.size) {index->
+                for (item in items) {
+                    Circulo(item)
+                }
+            }
         }
     }
 }
 
-fun Texto(){
-        Row(){
-            Texto("cr",
-                Color.Blue,
-                Color.Red
-
-            )
+@Composable
+fun Texto(texto:String, fondo: Color,
+          colorLetra: Color){
+    Text(
+        text=texto,
+        color=colorLetra,
+        fontSize=70.sp,
+        modifier= Modifier
+            .background(fondo)
+    )
 }
+
+@Composable
+fun Circulo(color:Color){
+    Box(
+        modifier= Modifier
+            .background(color, shape = CircleShape)
+            .size(70.dp)
+
+    )
 }
-
-
-
-
-
-
