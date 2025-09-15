ffa0c74be0e8bc4011932bd31247164c4a18b4be
EMMYT555 <130493792+EMMYT555@users.noreply.github.com>
2025-09-14 22:43:58 -0600

    Se añadio un comentario con nombre y numero de control

---
+++ app/src/main/java/com/example/segundodia/components/BarComponents.kt
@@ -0,0 +1,31 @@
+package com.example.segundodia.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TitleBar(name:String){
+    Text(
+        text = name,
+        fontSize = (25.sp),
+        color = Color.White
+    )
+}
+
+@Composable
+fun ActionButton(){
+    FloatingActionButton(onClick = {},
+        containerColor = Color.Red,
+        contentColor = Color.White) {
+
+        Image(imageVector = Icons.Default.Add,
+            contentDescription = "Más")
+    }
+
+}
---
+++ app/src/main/java/com/example/segundodia/view/ContentHomeView.kt
@@ -0,0 +1,8 @@
+package com.example.segundodia.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ContentHomeView(){
+
+}
