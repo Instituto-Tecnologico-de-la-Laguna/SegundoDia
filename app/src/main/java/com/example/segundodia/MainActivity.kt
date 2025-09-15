5fa71075a53ee0630b65a6cdb90a4158b27d0e4b
Mxryz <maryjosedoblas@gmail.com>
2025-09-15 13:22:31 -0600

    navegacion

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -4,29 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.segundodia.ui.theme.SegundoDiaTheme
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,19 +31,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val focusManager = LocalFocusManager.current
-                    val items = listOf(
-                        Color.Blue,
-                        Color.Black,
-                        Color.Cyan,
-                        Color.Yellow,
-                        Color.Red,
-                        Color.Gray,
-                        Color.Green,
-                        Color.DarkGray,
-                        Color.Magenta
-                    )
-                    MainScreen(items)
+
+
                 }
 
             }
@@ -59,56 +42,36 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun MainScreen(items : List<Color>) {
+fun TextView(texto:String) {
+    Text(text=texto,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold,
+        color=Color.Black)
+}
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row() {
-            Image(
-                painter = painterResource(id = R.drawable.criss),
-                contentDescription = null
-            )
-        }
-        Row(){
-            Texto("Clorox",
-                Color.Blue,
-                Color.White)
-            Spacer(modifier = Modifier.padding(10.dp))
-            Texto ("Precio",
-                Color.Red,
-                Color.White)
-        }
-        Spacer(modifier = Modifier.padding(20.dp))
-        LazyRow() {
-            items(items.size) {index->
-                for (item in items) {
-                    Circulo(item)
-                }
-            }
-        }
+@Composable
+fun Space(espacio:Int){
+    Spacer(modifier=Modifier.height(height = espacio.dp))
+}
+
+@Composable
+fun MainButton(name:String,backColor:Color,color:Color,onClick:()-> Unit){
+    Button(onClick=onClick,
+        colors= ButtonDefaults.buttonColors(
+            contentColor = color,
+            containerColor = backColor
+        )){
+        Text(text=name)
     }
+
 }
 
-@Composable
-fun Texto(texto:String, fondo: Color,
-          colorLetra: Color){
-    Text(
-        text=texto,
-        color=colorLetra,
-        fontSize=70.sp,
-        modifier= Modifier
-            .background(fondo)
-    )
-}
 
-@Composable
-fun Circulo(color:Color){
-    Box(
-        modifier= Modifier
-            .background(color, shape = CircleShape)
-            .size(48.dp)
 
-    )
-}
+
+
+
+
+
+
+
