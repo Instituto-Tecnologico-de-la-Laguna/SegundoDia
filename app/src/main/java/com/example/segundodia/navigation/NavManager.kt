333224fe1868085d4247254b256f5d74f8c2f888
jmot4 <jmota@gtech-services.com>
2025-09-17 14:00:18 -0600

    Se agregan funciones de navegacion

--- app/src/main/java/com/example/segundodia/navigation/NavManager.kt
+++ app/src/main/java/com/example/segundodia/navigation/NavManager.kt
@@ -3,10 +3,25 @@ package com.example.segundodia.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+import androidx.navigation.compose.rememberNavController
+import com.example.segundodia.view.DetailsView
+import com.example.segundodia.view.HomeView
+
 
 @Composable
 fun NavManager(){
-    val navController: NavHostController = rememberNavController()
+    val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "HomeView")
+    NavHost(navController = navController,
+        startDestination = "Home"){
+        composable("Home"){
+            HomeView(navController)
+        }
+        composable("Detail"){
+            DetailsView()
+        }
+
+    }
 }
