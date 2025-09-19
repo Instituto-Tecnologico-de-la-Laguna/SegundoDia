e1fde870495bcc2042b942a00d90ec5c312eff7b
jmot4 <jmota@gtech-services.com>
2025-09-19 13:48:58 -0600

    Se agrega Splashscreen a Segundo dia

--- app/src/main/java/com/example/segundodia/MainActivity.kt
+++ app/src/main/java/com/example/segundodia/MainActivity.kt
@@ -33,6 +33,7 @@ import com.example.segundodia.navigation.NavManager
 
 import com.example.segundodia.ui.theme.SegundoDiaTheme
 import com.example.segundodia.view.HomeView
+import com.example.segundodia.view.SplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +44,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SegundoDiaTheme {
                 NavManager()
+                //SplashScreen()
         }
     }
     }
---
+++ app/src/main/java/com/example/segundodia/view/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.example.segundodia.view
+
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import com.example.segundodia.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    LaunchedEffect(key1 = true) {
+        delay(1000)
+        navController.navigate("Home") {
+            popUpTo("Home") {
+            }
+        }
+    }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.clorox),
+                contentDescription = "Logo"
+            )
+        }
+    }
