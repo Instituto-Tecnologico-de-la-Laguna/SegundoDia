b16b3a7edc733737e32d022cb750818db874cbff
Emi-22 <emilianomohdz@gmail.com>
2025-09-30 11:27:59 -0600

    Cambios

--- app/src/main/java/com/example/segundodia/view/SplashScreen.kt
+++ app/src/main/java/com/example/segundodia/view/SplashScreen.kt
@@ -1,17 +1,20 @@
 package com.example.segundodia.view
 
-import android.window.SplashScreen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import com.example.segundodia.R
 import kotlinx.coroutines.delay
+import android.graphics.BitmapFactory
 
 @Composable
 fun SplashScreen(navController: NavController) {
@@ -21,13 +24,19 @@ fun SplashScreen(navController: NavController) {
             popUpTo("Splash") { inclusive = true }
         }
     }
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.clorox),
-                contentDescription = "Logo"
-            )
-        }
+    val context = LocalContext.current
+    val logo: ImageBitmap = remember {
+        BitmapFactory.decodeStream(
+            context.resources.openRawResource(R.raw.nfl_logo)
+        ).asImageBitmap()
     }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            bitmap = logo,
+            contentDescription = "NFL Logo"
+        )
+    }
+}
